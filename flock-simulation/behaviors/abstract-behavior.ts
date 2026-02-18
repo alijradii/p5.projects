1fb16fe6a12b55d12cad35e8379d75a6ad65c33d
Ali Jradi <alijradi2003@gmail.com>
2026-02-18 14:30:36 +0200

    [flock simulation] added abstract behavior class

---
+++ flock-simulation/behaviors/abstract-behavior.ts
@@ -0,0 +1,14 @@
+import { Vector2 } from "@shared/math/Vector2";
+import p5 from "p5";
+import { Flock } from "../flock";
+import { FlockAgent } from "../flock-agent";
+
+export abstract class AbstractBehavior {
+    weight: number;
+
+    constructor(weight: number) {
+        this.weight = weight;
+    }
+
+    abstract steer(agent: FlockAgent, flock: Flock, p: p5): Vector2;
+}
